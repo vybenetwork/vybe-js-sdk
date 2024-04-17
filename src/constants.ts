fbadf870f452b1d3946fb15a86a92a4b918a5cc7
Matt <matthew@vybenetwork.com>
2024-04-17 08:41:47 -0700

    Implement purchaseCredits

--- src/constants.ts
+++ src/constants.ts
@@ -1,8 +1,16 @@
+// TODO: replace these with the live versions
+export const VYBE_URL='http://localhost:8080'
+export const CREDITS_PRICE_USDC=5_000_000
+export const VYBE_WALLET="9XeDuDohkKyQCMXmkCu89u6nudRTkdpJN93v53KLGwnm"
+export const USDC_MINT='Gh9ZwEmdLJ8DscKNTkTqPbNwLNNBjuSzaG9Vp2KGtKJr'
+
+// Known constants
 export const STORAGE_KEY='vybe-data'
-export const VYBE_URL='http://localhost:8080' // TODO: replace with the live service url
 export const AUTH_ENDPOINT=`${VYBE_URL}/authenticate/wallet`
 export const PURCHASE_ENDPOINT=`${VYBE_URL}/credits/purchase/verify`
 export const AUTH_MSG="Please sign this message to authenticate with your Vybe account."
 
 // Errors
+export class NotAuthenticatedError extends Error {}
 export class AuthFailedError extends Error {}
+export class CreditPurchaseError extends Error {}
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,10 @@
-import { PublicKey } from "@solana/web3.js";
+import { Connection, PublicKey, Transaction } from "@solana/web3.js";
 import { loadAuthData, storeAuthData } from "./store";
 import { isBrowser, resolver } from "./utils";
-import { AUTH_ENDPOINT, AUTH_MSG, AuthFailedError } from "./constants";
+import { AUTH_ENDPOINT, AUTH_MSG, AuthFailedError, CREDITS_PRICE_USDC, CreditPurchaseError, NotAuthenticatedError, PURCHASE_ENDPOINT, USDC_MINT, VYBE_WALLET } from "./constants";
 import bs58 from 'bs58'
-import { BaseMessageSignerWalletAdapter, BaseSignerWalletAdapter } from "@solana/wallet-adapter-base";
-
-type Adapter = BaseMessageSignerWalletAdapter | BaseSignerWalletAdapter
+import { BaseMessageSignerWalletAdapter, BaseSignerWalletAdapter, WalletNotConnectedError, WalletSendTransactionError } from "@solana/wallet-adapter-base";
+import { TOKEN_PROGRAM_ID, createTransferInstruction, getAssociatedTokenAddress } from "@solana/spl-token";
 
 interface Account {
   id: string // UUID,
@@ -33,7 +32,7 @@ async function sendAuthData(body: AuthRequest): Promise<Account> {
   return data?.json()
 }
 
-async function auth(adapter: Adapter): Promise<Account | AuthFailedError> {
+async function auth(adapter: BaseMessageSignerWalletAdapter): Promise<Account | AuthFailedError> {
   if (!adapter.connected) return new AuthFailedError()
 
   const authData = await loadAuthData()
@@ -48,7 +47,7 @@ async function auth(adapter: Adapter): Promise<Account | AuthFailedError> {
     }
   } else {
     const input = Uint8Array.from(AUTH_MSG.split('').map((x) => x.charCodeAt(0)))
-    const output = await (adapter as BaseMessageSignerWalletAdapter).signMessage(input)
+    const output = await adapter.signMessage(input)
     requestBody.sig = bs58.encode(output)
     requestBody.generate = true
   }
@@ -70,12 +69,79 @@ async function auth(adapter: Adapter): Promise<Account | AuthFailedError> {
   return resp
 }
 
-interface VybeApi {
-  auth(signer: Adapter): Promise<Account | AuthFailedError>  
+async function submitTx(adapter: BaseSignerWalletAdapter, connection: Connection): Promise<string> {
+  if (!adapter.publicKey) throw new WalletNotConnectedError()
+
+  const toPk = new PublicKey(VYBE_WALLET)  
+  const mint = new PublicKey(USDC_MINT)
+
+  const fromUsdcAccount = await getAssociatedTokenAddress(mint, adapter.publicKey)
+  const toUsdcAccount = await getAssociatedTokenAddress(mint, toPk)
+
+  const tx = new Transaction()
+
+  // Check if the wallet has a USDC account.
+  // This will throw if they don't and the consumer should handle it.
+  await connection.getAccountInfo(fromUsdcAccount)
+
+  // Create the USDC transfer instruction
+  tx.add(
+    createTransferInstruction(
+      fromUsdcAccount,
+      toUsdcAccount,
+      adapter.publicKey,
+      CREDITS_PRICE_USDC,
+      [],
+      TOKEN_PROGRAM_ID
+    )
+  )
+
+  // Sign the TX
+  await adapter.signTransaction(tx)
+  // Submit the TX
+  const [err, sig] = await resolver(connection.sendRawTransaction(tx.serialize()))
+  if (err) throw err
+
+  return sig!
 }
 
-export async function init(): Promise<VybeApi> {
+async function processCreditPurchase(tx: String, generate = false): Promise<Account> {
+  const isoFetch = isBrowser ? fetch : () => Promise.reject('Node is not yet supported.') // support node
+  
+  const [err, resp] = await resolver(isoFetch(PURCHASE_ENDPOINT, {
+    method: 'POST',
+    body: JSON.stringify({ tx, generate })
+  }))
+
+  if (err || !resp ) throw new CreditPurchaseError()
+  
+  const account: Account = await resp.json()
+  return account
+}
+
+const purchaseCreditsFactory = (connection: Connection)  => {
+  return async function purchaseCredits(adapter: BaseSignerWalletAdapter): Promise<Account> {
+    const authData = await loadAuthData()
+    if (!authData) throw new NotAuthenticatedError()
+
+    const [err, tx] = await resolver(submitTx(adapter, connection))
+    if (err || !tx) throw err || new WalletSendTransactionError()
+
+    const [aerr, account] = await resolver(processCreditPurchase(tx, !authData.key))
+    if (aerr || !account) throw new CreditPurchaseError()
+   
+    return account
+  }
+}
+
+interface VybeApi {
+  auth(signer: BaseMessageSignerWalletAdapter): Promise<Account | AuthFailedError>
+  purchaseCredits(adapter: BaseSignerWalletAdapter): Promise<Account | CreditPurchaseError | NotAuthenticatedError>
+}
+
+export async function init(connection: Connection): Promise<VybeApi> {
   return {
-    auth
+    auth,
+    purchaseCredits: purchaseCreditsFactory(connection)
   }
 }
