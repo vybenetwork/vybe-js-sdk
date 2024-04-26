eca99dbc1051e446b9dc78ee66fdd2562a50c85c
MattXYZ <matt@crcls.so>
2024-04-26 15:58:44 -0700

    Convert the repo to a mono-repo

--- src/utils.ts
+++
@@ -1,6 +0,0 @@
-export const isBrowser = typeof window !== 'undefined' && typeof window.document !== 'undefined'
-
-export type ResolverTuple<T> = [Error | undefined, T?]
-
-export const resolver = <T>(promise: Promise<T>): Promise<ResolverTuple<T>> =>
-  promise.then((data: T): ResolverTuple<T> => [undefined, data]).catch((e: Error): ResolverTuple<T> => [e])
