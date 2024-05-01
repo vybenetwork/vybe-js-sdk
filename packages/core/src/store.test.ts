3661e80e54565b2649d2d2015d6733699c093579
Pablo Larrea <larrea.pablo@gmail.com>
2024-04-30 17:33:38 -0700

    ANLS-2614: Implement a test suite for the packages. (#7)
    * core test setup
    
    * react test setup
    
    * store tests
    
    * react testw
    
    * test commands
    
    * fixes after npm audit
    
    * update lerna
    
    ---------
    
    Co-authored-by: Pablo Larrea <plarrea@membranelabs.com>

---
+++ packages/core/src/store.test.ts
@@ -0,0 +1,22 @@
+import { loadAuthData, storeAuthData } from "./store"
+import { TextDecoder, TextEncoder } from 'util'
+
+Object.assign(global, { TextDecoder, TextEncoder });
+jest.mock('./constants', () => ({ STORAGE_KEY: 'vybe-data' }));
+
+const authData = {pk: 'somePk', sig: 'someSig', key: 'someKey'}
+const encodedAuthData = 'eyJwayI6InNvbWVQayIsInNpZyI6InNvbWVTaWciLCJrZXkiOiJzb21lS2V5In0='
+
+describe('Store', () => {
+  test('storeAuthData stores value in localStorage', async () => {
+    jest.spyOn(window.localStorage.__proto__, 'setItem')
+    storeAuthData(authData)
+    expect(window.localStorage.setItem).toHaveBeenCalledWith( 'vybe-data', encodedAuthData)
+  })
+
+  test('loadAuthData returns value from localStorage', async () => {
+    jest.spyOn(window.localStorage.__proto__, 'getItem').mockReturnValue(encodedAuthData)
+    const data = await loadAuthData()
+    expect(data).toEqual(authData)
+  })
+})
---
+++ packages/react/src/provider.test.tsx
@@ -0,0 +1,27 @@
+import { render } from '@testing-library/react';
+import { useContext } from 'react';
+import VybeProvider from './provider';
+import VybeContext from './context';
+import { VybeWidget } from './types';
+
+const TestingComponent = () => {
+  const { account } = useContext(VybeContext);
+  return (
+    <p data-testid="account-id">account id: {account?.id}</p>
+  );
+};
+
+describe('<VybeProvider />', () => {
+
+  test('Context returns value of account', () => {
+    const { getByTestId } = render(
+      <VybeProvider config={{widgets: [VybeWidget.ReverseNFTSearch]}}>
+        <TestingComponent />
+      </VybeProvider>,
+    );
+
+    const account = getByTestId('account-id');
+    // todo: update when account is available
+    expect(account.textContent).toEqual('account id: ');
+  });
+})
