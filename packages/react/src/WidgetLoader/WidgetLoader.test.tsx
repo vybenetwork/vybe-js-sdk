8e652d15415ecb2a9ca0f31df20a2ca396209a8b
Pablo Larrea <plarrea@membranelabs.com>
2024-05-02 11:08:47 -0700

    fix widget loader test

--- packages/react/src/WidgetLoader/WidgetLoader.test.tsx
+++ packages/react/src/WidgetLoader/WidgetLoader.test.tsx
@@ -3,14 +3,17 @@ import { render } from '@testing-library/react'
 import VybeProvider from 'src/provider'
 import { VybeWidget } from 'src/types'
 import WidgetLoader from './WidgetLoader'
+import { Connection } from '@solana/web3.js'
 
 jest.mock('src/widgets/constants')
 
 describe('<WidgetLoader />', () => {
+  const connection = new Connection('http://localhost:8899')
+
   test('Returns loaded widget', async () => {
     const { getByTestId } = await act(() =>
       render(
-        <VybeProvider config={{ widgets: [VybeWidget.ReverseNFTSearch] }}>
+        <VybeProvider config={{ widgets: [VybeWidget.ReverseNFTSearch] }} connection={connection}>
           <WidgetLoader widget={VybeWidget.ReverseNFTSearch} />
         </VybeProvider>
       )
@@ -22,7 +25,7 @@ describe('<WidgetLoader />', () => {
   test('Widget is not returned when not initialized', async () => {
     const { getByText } = await act(() =>
       render(
-        <VybeProvider config={{ widgets: [] }}>
+        <VybeProvider config={{ widgets: [] }} connection={connection}>
           <WidgetLoader widget={VybeWidget.ReverseNFTSearch} />
         </VybeProvider>
       )
